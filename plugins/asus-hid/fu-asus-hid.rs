5cbdb56f7f7494195de0dda812b53a9500f7e522
Mario Limonciello <mario.limonciello@amd.com>
2024-09-25 01:04:39 -0500

    add some more defines to assist in looking at dumps for patterns

--- plugins/asus-hid/fu-asus-hid.rs
+++ plugins/asus-hid/fu-asus-hid.rs
@@ -3,9 +3,18 @@
 
 #[repr(u32)]
 enum FuAsusHidCommand {
-    Version = 0x00310305,
-    Version2 = 0x00310405,
+    SwitchToRom = 0xd2, // Call with SetFeature, size 0x40
+    SomethingCaps = 0x49d,
+    PreUpdateSomething =  0x200005,
+    PreUpdateSomething2 = 0x200908,
+    PreUpdateSomething3 = 0x260b08,
+    PreUpdateSomething4 = 0x9332e0,
+    GetFwConfig = 0x312005, // Call with SetFeature, size 0x40
+    Version = 0x00310305, // called GetFWVersion
+    Version2 = 0x00310405,  //called main fw version
     Manufacturer = 0x53555341,
+    FlashReset = 0xc400, //Call with SetFeature, size 0x40, wait 5000ms
+    FlashTaskSomething = 0xd000,
 }
 
 #[repr(u8)]
